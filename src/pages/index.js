fbf6ca2c3e2e44ba56ef6906348e8de06f4c291e
Joao Pedro Renan <jota.renan@gmail.com>
2019-12-14 18:58:30 -0300

    Criaçao, indexacao e renderizacao de paginas de posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import PostPreview from '../components/post-preview';
+import usePosts from '../hooks/use-posts';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello World!</p>
-    <Link to="/about">sobre mim &rarr;</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello World!</p>
+      <Link to="/about">sobre mim &rarr;</Link>
+
+      <h2>Leia meu blog!</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
